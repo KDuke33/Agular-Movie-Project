95c33386af7eb731344c3eec16d2a279164e0c69
Nicole <nbush89@gmail.com>
2021-01-30 21:33:56 -0500

    merging code

--- src/app/service/movie-service.service.ts
+++ src/app/service/movie-service.service.ts
@@ -2,45 +2,47 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieServiceService {
-
   movData: any;
-  
-  apiKey:string = 'fc7b8d6a3fc62e195f60b003d5b55b5a'
 
-  apiURL: string = 'https://api.themoviedb.org/3/discover/movie?'
+  apiKey: string = 'fc7b8d6a3fc62e195f60b003d5b55b5a';
 
-  constructor(private http: HttpClient) { }
+  apiURL: string = 'https://api.themoviedb.org/3/discover/movie?';
 
-  setMovData(data:any){
-    this.movData
+  constructor(private http: HttpClient) {}
+
+  setMovData(data: any) {
+    this.movData;
   }
-  getmovData(data:any ){
-    return this.movData
+  getmovData(data: any) {
+    return this.movData;
   }
   getMovies(genre?: any, year?: any, rating?: any): Observable<any> {
-    let searchParams = {apiKey: this.apiKey, genre: "", primary_release_year: "", ["vote_average.gte"]: ""}
-    if(year){
-      searchParams.primary_release_year = year
+    let searchParams = {
+      apiKey: this.apiKey,
+      genre: '',
+      primary_release_year: '',
+      ['vote_average.gte']: '',
+    };
+    if (year) {
+      searchParams.primary_release_year = year;
     }
-    if(genre){
-      searchParams.genre = genre
+    if (genre) {
+      searchParams.genre = genre;
     }
-    if(rating){
-      searchParams["vote_average.gte"] = rating
+    if (rating) {
+      searchParams['vote_average.gte'] = rating;
     }
     return this.http.get(this.apiURL, {
       params: searchParams,
-    })
+    });
   }
-  getMovieTitle(title: string){
-    return this.http.get("https://api.themoviedb.org/3/search/movie", {
-      params: {api_key: this.apiKey, query: title}
-    })
+  getMovieTitle(title: string) {
+    return this.http.get('https://api.themoviedb.org/3/search/movie', {
+      params: { api_key: this.apiKey, query: title },
+    });
   }
-  // ${this.apiURL}vote_average.gte=${rating}
 }
