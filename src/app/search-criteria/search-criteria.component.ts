ead5ac189f2456f345bf44a25df36d352ab73b50
aspencer15 <amycspencer@gmail.com>
2021-01-31 19:38:13 -0500

    updated form

--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -10,10 +10,14 @@ import { MovieServiceService } from '../service/movie-service.service';
   styleUrls: ['./search-criteria.component.css']
 })
 export class SearchCriteriaComponent implements OnInit {
+  genre: any;
 
     constructor(private service: MovieServiceService, private router: Router) { }
   
     ngOnInit(): void {
+      this.service.getGenres().subscribe(response => {
+        this.genre = response["genres"];
+      });
     }
    
 
@@ -22,10 +26,10 @@ export class SearchCriteriaComponent implements OnInit {
     this.router.navigate(["movie-list"], {queryParams: 
       {title: form.value.title, 
         year: form.value.year, 
-        // genre: form.value.genre, 
+        genre: form.value.genre, 
         rating: form.value.rating
       }});
-        console.log("something happened")
+        console.log("something happened");
 
   }
 }
--- src/app/service/movie-service.service.ts
+++ src/app/service/movie-service.service.ts
@@ -9,6 +9,8 @@ import { Observable } from 'rxjs';
 export class MovieServiceService {
 
   movData: any;
+
+  dataGenres = [];
   
   apiKey:string = 'fc7b8d6a3fc62e195f60b003d5b55b5a'
 
@@ -23,6 +25,7 @@ export class MovieServiceService {
     return this.movData
   }
   getMovies(genre?: any, year?: any, rating?: any): Observable<any> {
+    
     let searchParams = {apiKey: this.apiKey, genre: "", primary_release_year: "", ["vote_average.gte"]: ""}
     if(year){
       searchParams.primary_release_year = year
@@ -41,6 +44,12 @@ export class MovieServiceService {
     return this.http.get("https://api.themoviedb.org/3/search/movie", {
       params: {api_key: this.apiKey, query: title}
     })
+  }
+
+  getGenres() {
+    return this.http.get("https://api.themoviedb.org/3/genre/movie/list?", {
+      params: { api_key: this.apiKey }
+    });
   }
   // ${this.apiURL}vote_average.gte=${rating}
 }
